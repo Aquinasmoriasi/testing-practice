c517e14402aa468a8a965139a863c37ad2dfd2bc
unknown <aquinasmoriasi@gmail.com>
2022-07-20 11:46:58 +0300

    Add test for reversed strings

--- index.test.js
+++ index.test.js
@@ -1,6 +1,5 @@
-const stringLength = (string) => string.split('').length;
-
 describe('your string,', () => {
+  const stringLength = (string) => string.split('').length;
   test('is 3 letters long', () => {
     expect(stringLength('dog')).toBe(3);
   });
@@ -9,5 +8,23 @@ describe('your string,', () => {
   });
   test('is not longer than 10 chracters', () => {
     expect(stringLength('client')).toBeLessThanOrEqual(10);
+  });
+});
+
+describe('your string,', () => {
+  const reverseStr = (string) => string
+    .split('')
+    .reverse()
+    .join('');
+
+  test('was reversed', () => {
+    expect(reverseStr('crop')).toBe('porc');
+  });
+
+  test('was reversed', () => {
+    expect(reverseStr('song')).toBe('gnos');
+  });
+  test('was reversed', () => {
+    expect(reverseStr('index')).toBe('xedni');
   });
 });
