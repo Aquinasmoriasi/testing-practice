36d40910a5c375a32447dea97f88b8d0b36bc411
unknown <aquinasmoriasi@gmail.com>
2022-07-20 12:43:22 +0300

    Add calculator function

--- index.test.js
+++ index.test.js
@@ -16,7 +16,6 @@ describe('your string,', () => {
     .split('')
     .reverse()
     .join('');
-
   test('was reversed', () => {
     expect(reverseStr('crop')).toBe('porc');
   });
@@ -26,5 +25,61 @@ describe('your string,', () => {
   });
   test('was reversed', () => {
     expect(reverseStr('index')).toBe('xedni');
+  });
+});
+
+describe('The answer is,', () => {
+  const calculator = {
+    add: (...args) => args.reduce((a, b) => a + b),
+    subtract: (a, b) => a - b,
+    multiply: (...args) => args.reduce((a, b) => a * b),
+    divide: (a, b) => a / b,
+  };
+
+  test('the sum of the numbers,', () => {
+    expect(calculator.add(2, 3, 1)).toBe(6);
+  });
+
+  test('the sum of the numbers,', () => {
+    expect(calculator.add(-2, 3, -1)).toBe(0);
+  });
+
+  test('the sum of the numbers,', () => {
+    expect(calculator.add(1, 16)).toBe(17);
+  });
+
+  test('the product of the numbers,', () => {
+    expect(calculator.multiply(2, 2, 2)).toBe(8);
+  });
+
+  test('the product of the numbers,', () => {
+    expect(calculator.multiply(3, 0)).toBe(0);
+  });
+
+  test('the product of the numbers,', () => {
+    expect(calculator.multiply(4, -2)).toBe(-8);
+  });
+
+  test('the difference between the two numbers,', () => {
+    expect(calculator.subtract(3, 2)).toBe(1);
+  });
+
+  test('the difference between the two numbers,', () => {
+    expect(calculator.subtract(3, 3)).toBe(0);
+  });
+
+  test('the difference between the two numbers,', () => {
+    expect(calculator.subtract(2, 8)).toBe(-6);
+  });
+
+  test('the division of the two numbers,', () => {
+    expect(calculator.divide(9, 0)).toBe(Infinity);
+  });
+  test('the division of the two numbers,', () => {
+    expect(calculator.divide(9, 3)).toBe(3);
+  });
+
+  test('the division of the two numbers,', () => {
+    expect(calculator.divide(-3, -3)).toBe(1);
   });
 });
