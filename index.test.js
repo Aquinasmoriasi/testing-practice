d45b9b5a1d558e4a6b60acc6c34770fb2b2ea31c
unknown <aquinasmoriasi@gmail.com>
2022-07-20 10:10:44 +0300

    add test for range

--- index.test.js
+++ index.test.js
@@ -1,7 +1,13 @@
 const stringLength = (string) => string.split('').length;
 
-describe('your string is', () => {
-  test('3 letters long', () => {
+describe('your string,', () => {
+  test('is 3 letters long', () => {
     expect(stringLength('dog')).toBe(3);
+  });
+  test('has at least one character', () => {
+    expect(stringLength('')).toBeFalsy();
+  });
+  test('is not longer than 10 chracters', () => {
+    expect(stringLength('client')).toBeLessThanOrEqual(10);
   });
 });
